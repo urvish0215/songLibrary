ede6524c465998ffd7604ac14c86b5f79fba8658
Urvish <Urvish@DESKTOP-O6QIF1C>
2019-02-20 14:17:10 -0500

    rename Main.java to SongLib

--- src/application/SongLib.java
+++ src/application/SongLib.java
@@ -6,7 +6,7 @@ import javafx.stage.Stage;
 import javafx.scene.Scene;
 
 
-public class Main extends Application {
+public class SongLib extends Application {
 	@Override
 	public void start(Stage primaryStage) throws Exception {
 		Stage window = primaryStage;
